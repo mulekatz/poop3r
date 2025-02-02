9c2203517c9064d0199245cbbb65254b32c1c146
mulekatz <mulekatz@gmail.com>
2025-02-02 22:06:46 +0100

    removed component from home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,5 @@
 import { useWallet } from "@vechain/dapp-kit-react";
 import UserCard from "./UserCard";
-import StatisticsCard from "./StatisticsCard";
 
 export default function Home() {
   const { account } = useWallet();
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <section className="flex flex-col w-full flex-1 items-center gap-4">
       <UserCard account={account} />
-      <StatisticsCard />
     </section>
   );
 }
