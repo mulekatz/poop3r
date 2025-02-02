90279bc0db367ac65d2079a8fa00388a527d046c
mulekatz <mulekatz@gmail.com>
2025-02-02 22:04:49 +0100

    changed paths for home, statistics and landingpage

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
 import Layout from "./Layout";
-import History from "./components/history/History.tsx";
+import Statistics from "./components/statistics/Statistics.tsx";
 import Settings from "./components/settings/Settings.tsx";
 import Info from "./components/info/Info.tsx";
-import Connect from "./components/connect/Connect.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import Home from "./components/home/Home.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/connect" element={<Connect />} />
+          <Route path="/" element={<App />} />
           <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<App />} />
-            <Route path="/history" element={<History />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/statistics" element={<Statistics />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/info" element={<Info />} />
           </Route>
