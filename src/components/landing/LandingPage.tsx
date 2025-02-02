e3d532d3be00be82cd4c320d17be74641b457e05
mulekatz <mulekatz@gmail.com>
2025-02-02 22:08:53 +0100

    added account check and redirect / connect component conditional

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -1,10 +1,18 @@
+import { useWallet } from "@vechain/dapp-kit-react";
 import Connect from "../connect/Connect";
 import Vechain from "../header/Vechain";
+import { Navigate } from "react-router";
 
 export default function LandingPage() {
+  const { account } = useWallet();
+
+  if (account) {
+    return <Navigate to="/home" replace />;
+  }
+
   return (
     <section className="flex flex-col w-full flex-1 gap-12 justify-between">
-      <Connect />
+      {!account && <Connect />}
       <Vechain />
     </section>
   );
