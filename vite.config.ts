6379d39cc9eb48add4d8810581d5c3e249987c91
mulekatz <mulekatz@gmail.com>
2025-02-02 20:56:41 +0100

    test Polyfills for package picasso

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,27 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), nodePolyfills()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      include: ["module"],
+      globals: {
+        process: true,
+        Buffer: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      // Füge Alias für problematische Module hinzu
+      "@vechain/picasso": "/node_modules/@vechain/picasso/dist/index.js",
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/node_modules\/@vechain\/picasso/],
+    },
+  },
 });
