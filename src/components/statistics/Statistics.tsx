cfa3502b4a5f77d5850b54a7b28c871e2a4a165b
mulekatz <mulekatz@gmail.com>
2025-02-02 22:06:29 +0100

    renamed component from history to statistics

---
+++ src/components/statistics/Statistics.tsx
@@ -0,0 +1,9 @@
+import StatisticsCard from "./StatisticsCard";
+
+export default function Statistics() {
+  return (
+    <section className="flex flex-col w-full flex-1 items-center gap-4">
+      <StatisticsCard />
+    </section>
+  );
+}
